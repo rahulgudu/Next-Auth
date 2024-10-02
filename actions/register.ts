d442aa3ed5982ac4d12de7a8c39a85ecd2683fbc
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-10-02 13:44:12 +0530

    create a user

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,36 @@
 "use server";
-
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import * as z from "zod";
 
-export const register = async (values: unknown) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
   if (!validateFields.success) {
     return { error: "Invalid fields!" };
   }
 
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email Already Exists" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Todo: Send verification token email
+
   return {
-    success: "Email sent!",
+    success: "User Registered",
   };
 };
