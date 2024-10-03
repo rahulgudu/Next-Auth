5bd5428d100ccd51e47a90d33b1919c646b60e4e
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-10-03 10:54:35 +0530

    login and logout functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,21 @@
+import { auth, signOut } from "@/auth";
 import React from "react";
 
-const Settings = () => {
-  return <div>Settings Page</div>;
+const Settings = async () => {
+  const session = await auth();
+  return (
+    <div>
+      Settings Page
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}>
+        <button type="submit">Sign Out</button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,25 @@
-import GitHub from "next-auth/providers/github";
+import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "./data/user";
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
